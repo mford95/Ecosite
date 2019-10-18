45cfd184a2615a15d41d7faff7fe41998a591323
Matthew <mjford95@gmail.com>
2019-10-19 00:36:51 +1300

    another page

--- main.js
+++ main.js
@@ -32,3 +32,9 @@ function myFunction() {
     x.className = "topnav";
   }
 }
+
+function toggleDarkLight() {
+  var body = document.getElementById("body");
+  var currentClass = body.className;
+  body.className = currentClass == "dark-mode" ? "light-mode" : "dark-mode";
+}
